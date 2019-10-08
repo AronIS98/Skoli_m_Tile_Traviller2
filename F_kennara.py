cec26f21af568315e2c105a8c7f49cbcbbf9726a
Bjarki Laxdal <bjarki18@ru.is>
2019-10-08 09:33:27 +0000

    yeet

--- F_kennara.py
+++ F_kennara.py
@@ -4,6 +4,7 @@ EAST = 'e'
 SOUTH = 's'
 WEST = 'w'
 
+
 def move(direction, col, row):
     ''' Returns updated col, row given the direction '''
     if direction == NORTH:
@@ -14,11 +15,13 @@ def move(direction, col, row):
         col += 1
     elif direction == WEST:
         col -= 1
-    return(col, row)    
+    return (col, row)
+
 
 def is_victory(col, row):
     ''' Return true is player is in the victory cell '''
-    return col == 3 and row == 1 # (3,1)
+    return col == 3 and row == 1  # (3,1)
+
 
 def print_directions(directions_str):
     print("You can travel: ", end='')
@@ -37,6 +40,7 @@ def print_directions(directions_str):
         first = False
     print(".")
 
+
 def lever(money):
     lever = input("Pull a lever (y/n): ")
     if lever == "y" or lever == "Y":
@@ -44,30 +48,31 @@ def lever(money):
         print("You received 1 coin, your total is now {}.".format(money))
     return money
 
-        
-def find_directions(col, row,coin):
+
+def find_directions(col, row, coin):
     ''' Returns valid directions as a string given the supplied location '''
-    if col == 1 and row == 1:   # (1,1)
+    if col == 1 and row == 1:  # (1,1)
         valid_directions = NORTH
-    elif col == 1 and row == 2: # (1,2)
-        valid_directions = NORTH+EAST+SOUTH
+    elif col == 1 and row == 2:  # (1,2)
+        valid_directions = NORTH + EAST + SOUTH
         coin = lever(coin)
-    elif col == 1 and row == 3: # (1,3)
-        valid_directions = EAST+SOUTH
-    elif col == 2 and row == 1: # (2,1)
+    elif col == 1 and row == 3:  # (1,3)
+        valid_directions = EAST + SOUTH
+    elif col == 2 and row == 1:  # (2,1)
         valid_directions = NORTH
-    elif col == 2 and row == 2: # (2,2)
-        valid_directions = SOUTH+WEST
+    elif col == 2 and row == 2:  # (2,2)
+        valid_directions = SOUTH + WEST
         coin = lever(coin)
-    elif col == 2 and row == 3: # (2,3)
-        valid_directions = EAST+WEST
+    elif col == 2 and row == 3:  # (2,3)
+        valid_directions = EAST + WEST
         coin = lever(coin)
-    elif col == 3 and row == 2: # (3,2)
-        valid_directions = NORTH+SOUTH
+    elif col == 3 and row == 2:  # (3,2)
+        valid_directions = NORTH + SOUTH
         coin = lever(coin)
-    elif col == 3 and row == 3: # (3,3)
-        valid_directions = SOUTH+WEST
-    return valid_directions,coin
+    elif col == 3 and row == 3:  # (3,3)
+        valid_directions = SOUTH + WEST
+    return valid_directions, coin
+
 
 def play_one_move(col, row, valid_directions):
     ''' Plays one move of the game
@@ -75,7 +80,7 @@ def play_one_move(col, row, valid_directions):
     victory = False
     direction = input("Direction: ")
     direction = direction.lower()
-    
+
     if not direction in valid_directions:
         print("Not a valid direction!")
     else:
@@ -83,21 +88,32 @@ def play_one_move(col, row, valid_directions):
         victory = is_victory(col, row)
     return victory, col, row
 
+
 # The main program starts here
 victory = False
 row = 1
 col = 1
 money = 0
+check = 1
 valid_directions = NORTH
 print_directions(valid_directions)
 
-while not victory:
+while check == 1:
     victory, col, row = play_one_move(col, row, valid_directions)
     if victory:
-        print("Victory! ",end = "")
+        print("Victory! ", end="")
         print("Total coins {}.".format(money))
+        continue_input = input('Play again (y/n): ')
+        continue_input.lower()
+        if continue_input is 'y':
+            money = 0
+            row = 1
+            col = 1
+            valid_directions = NORTH
+            print_directions(valid_directions)
+        else:
+            check = 0
 
     else:
-        valid_directions,money = find_directions(col, row ,money)
+        valid_directions, money = find_directions(col, row, money)
         print_directions(valid_directions)
-        
